522e2e80ba243ad1a59e186553b9dbc4911f7cc2
Diogo Ribeiro <pdcribeiro95@gmail.com>
2025-08-19 06:19:51 +0100

    add: reactive state experiment

---
+++ learning/misc/state.js
@@ -0,0 +1,113 @@
+function state(target) {
+    let path = []
+    const listeners = []
+    const proxy = new Proxy(() => { }, {
+        get(target_, prop, receiver_) {
+            console.debug('get', prop, path)
+            switch (prop) {
+                case '_isState': return true
+                case '_val': {
+                    const result = getCompute(target, path)()
+                    path = []
+                    return result
+                }
+            }
+            path.push({ prop })
+            return proxy
+        },
+        apply(target_, thisArg_, args) {
+            console.debug('apply', path.at(-1), args, path)
+            switch (path.at(-1).prop) {
+                case '_get': {
+                    const result = getCompute(target, path.slice(0, -1))()
+                    path = []
+                    return result
+                }
+                case '_listen': {
+                    const callback = args[0]
+                    const compute = getCompute(target, path.slice(0, -1))
+                    const update = () => callback(compute())
+                    listeners.push({ path, update })
+                    update()
+                    path = []
+                    return
+                }
+            }
+            path.push({ args })
+            return proxy
+        },
+        set(target_, prop, value) {
+            console.debug('set', prop, path)
+            // TODO: prevent setting outside of target (eg s.a.toUpperCase().b = 1) (break on function?)
+            const nested = path.reduce((o, p) => o[p.prop], target)
+            const result = Reflect.set(nested, prop, value)
+            // TODO: check if set on current path affects any listener
+            // TODO: handle breaking other paths (eg. s.user = null -> s.user.name breaks)
+            listeners.forEach(l => l.update())
+            path = []
+            return result
+        },
+    })
+    return proxy
+}
+
+function getCompute(target, path) {
+    return () => {
+        console.debug('compute', target, path)
+        let prevResult
+        return path.reduce((r, p) => {
+            console.debug({ r, p, prevResult })
+            const newResult = p.prop ? r[p.prop] : r.apply(prevResult, ...p.args)
+            prevResult = r
+            return newResult
+        }, target)
+    }
+}
+
+// DOM
+
+const tags = new Proxy({}, {
+    get(_, name) {
+        return tag.bind(undefined, name)
+    }
+})
+
+function tag(name, ...args) {
+    const [props, ...children] = args.length && Object.getPrototypeOf(args[0]) === Object.prototype ? args : [{}, ...args]
+    const dom = document.createElement(name)
+    for (let [k, v] of Object.entries(props)) {
+        // TODO
+    }
+    add(dom, children)
+    return dom
+}
+
+function add(dom, ...children) {
+    for (const c of children.flat(Infinity)) {
+        if (c._isState) {
+            const node = document.createTextNode('aa')
+            c._listen((v) => node.textContent = v)
+            dom.append(node)
+        } else {
+            dom.append(c)
+        }
+    }
+}
+
+// TEST
+
+var s = state({
+    user: {
+        name: '',
+    },
+})
+
+const input = tags.input()
+input.oninput = (e) => s.user.name = e.target.value
+
+const app = tags.div(
+    input,
+    tags.h1('hello ', s.user.name),
+)
+
+add(document.body, app)
